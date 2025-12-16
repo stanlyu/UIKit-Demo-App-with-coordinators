5437702797b10f0d3633ed30e66f91b49ddd6045
Stanislav Lyubchenko <lyubchenko.s3@wb.ru>
2025-12-16 19:36:46 +0300

    Initial Commit

---
+++ Demo App With Coordinators/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Demo App With Coordinators
+//
+//  Created by Любченко Станислав Валерьевич on 16.12.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
